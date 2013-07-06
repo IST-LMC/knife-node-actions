7691cf5bd7411a98c466df657e3ae0543fee318d
David Ackerman <david.s.ackerman@gmail.com>
2013-07-06 15:01:39 -0600

    Adding stubs for listing and running actions.

---
+++ lib/chef/knife/node_action_list.rb
@@ -0,0 +1,12 @@
+class Chef
+	class Knife
+		class NodeActionList < Knife
+
+			banner "knife node action list (options)"
+
+			def run
+
+			end
+		end
+	end
+end
---
+++ lib/chef/knife/node_action_run.rb
@@ -0,0 +1,12 @@
+class Chef
+	class Knife
+		class NodeActionRun < Knife
+
+			banner "knife node action run NODE_ACTION (options)"
+
+			def run
+
+			end
+		end
+	end
+end
